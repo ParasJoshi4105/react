22842644e80e73eeddc79f513e2e23f436cfca3e
ParasJoshi4105 <pjoshi4105@gmail.com>
2023-12-29 00:45:38 +0530

    Version-2-29-12-2023

--- src/App.js
+++ src/App.js
@@ -1,52 +1,58 @@
-// import logo from './logo.svg';
 
-import  React ,  { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-// import About from './components/About';
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
-  const [alert , setAlert] = useState(null);
+  const [alert, setAlert] = useState(null);
 
-  const showAlert = (message , type) => {
+  const showAlert = (message, type) => {
 
-      setAlert({
-        message : message,
-        type : type,
-      })
-      
-      setTimeout(() => {
-        setAlert(null)
-      }, 1500);
+    setAlert({
+      message: message,
+      type: type,
+    })
+
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
 
   }
 
-  const[darkmode , setDarkmode] = useState({
+  const [darkmode, setDarkmode] = useState({
 
-      background : 'light',
-      color : 'black'
-  }); 
+    background: 'light',
+    color: 'black'
+  });
 
   const toggleMode = () => {
 
-    if(darkmode.background === "light"){
+    if (darkmode.background === "light") {
 
       setDarkmode({
-        background : "dark",
-        color : "white"
+        background: "dark",
+        color: "white"
       });
-      showAlert("Dark Mode Has Been Enabled" , "success");
+      showAlert("Dark Mode Has Been Enabled", "success");
       document.body.style.backgroundColor = "#241f35"
+      document.title = "TextUtils - Darkmode"
     }
     else {
       setDarkmode({
-        background : "light",
-        color : "black"
+        background: "light",
+        color: "black"
       });
-      showAlert("Light Mode Has Been Enabled" , "success");
+      showAlert("Light Mode Has Been Enabled", "success");
       document.body.style.backgroundColor = "#FFFFFF"
+      document.title = "TextUtils - LightMode"
     }
 
 
@@ -54,36 +60,25 @@ function App() {
 
   return (
     <>
+      <Router>
 
 
-      {/* <div classNameName="headerWrapper">
-        <div classNameName="headerFlex">
-          <div classNameName="headerNavigationLink">
-            <ul>
-              <li>
-                <p>Home</p>
-              </li>
-              <li>
-                <p>About</p>
-              </li>
-              <li>
-                <p>Contact</p>
-              </li>
-              <li>
-                <p>Carrer</p>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div> */}
-      <NavBar title="My App" navlink="About Us" mode={darkmode.background} color={darkmode.color} toggleMode={toggleMode} />
-      {/* <Alert alertMessage="Hello Paras , How Are You , I Hope You Are Doing Good"></Alert> */}
-      <Alert alertMessage={alert}></Alert>
-      {/* <NavBar/> */}
-      <div className="container">
-        <TextForm heading="Welcome To TextUtils " label="Enter The Text Below"  mode={darkmode.background} color={darkmode.color} />
-        {/* <About></About> */}
-      </div>
+        <NavBar title="My App" navlink="About Us" mode={darkmode.background} color={darkmode.color} toggleMode={toggleMode} />
+
+        <Alert alertMessage={alert}></Alert>  
+        <div className="container">
+
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>   
+            <Route path="/">
+              <TextForm heading="Welcome To TextUtils " label="Enter The Text Below" mode={darkmode.background} color={darkmode.color} />
+            </Route>
+          </Switch>
+
+        </div >
+      </Router>
     </>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import { Link } from 'react-router-dom'
+// import {Link} from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 export default function NavBar(props) {
   return (
     <>
       <nav className={`navbar navbar-expand-lg bs-body-bg    bg-${props.mode}`}>
         <div className="container-fluid">
-          <a className={`navbar-brand text-${props.color}`} href="/"> {props.title} </a>
+          <Link className={`navbar-brand text-${props.color}`} to="/"> {props.title} </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className={`nav-link text-${props.color}`} aria-current="page" href="/components/About.js">Home</a>
+                <Link className={`nav-link text-${props.color}`} aria-current="page" to="/">Home</Link>
               </li>
               <li className="nav-item">
-                <a className={`nav-link text-${props.color}`} href="/">{props.navlink}</a>
+                <Link className={`nav-link text-${props.color}`} to="/about">{props.navlink}</Link>
               </li>
             </ul>
 
